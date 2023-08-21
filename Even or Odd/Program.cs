89a2411f04b99e4ff42de2fbee0fc315f48e3ccd
Jelena077 <jelenaasavicc@googlemail.com>
2023-08-21 21:02:24 +0200

    Add project files.

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter any number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            CheckNumIsEvenOrOdd(number);
+            Console.ReadLine();
+
+        }
+
+        public static void CheckNumIsEvenOrOdd(int _number)
+        {
+            if (_number % 2 == 0)
+            {
+                Console.WriteLine("Number {0} is Even", _number);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} is Odd", _number);
+            }
+        }
+
+    }
+
+}
